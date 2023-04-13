01bdd38eea145817bd3b89676bcef5013cfdd35e
Fathzer <jm-dev@astesana.net>
2023-04-13 15:36:52 +0200

    Adapts the jdbbackup-core changes

--- src/main/java/com/fathzer/jdbbackup/cmd/ExtensionSettingsConverter.java
+++ src/main/java/com/fathzer/jdbbackup/cmd/ExtensionSettingsConverter.java
@@ -1,27 +1,22 @@
 package com.fathzer.jdbbackup.cmd;
 
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
-import com.fathzer.jdbbackup.JDbBackup;
 import com.fathzer.plugin.loader.utils.FileUtils;
 
 import picocli.CommandLine.ITypeConverter;
 
 /** A converter that loads the plugins in a jar file whose path is passed as value.
  */
-public class ExtensionSettingsConverter implements ITypeConverter<URLClassLoader> {
+public class ExtensionSettingsConverter implements ITypeConverter<List<Path>> {
 	@Override
-	public URLClassLoader convert(String value) throws Exception {
-		final URL[] urls = FileUtils.getJarFiles(Paths.get(value), 1).stream().map(FileUtils::getURL).toArray(URL[]::new);
-		if (urls.length==0) {
+	public List<Path> convert(String value) throws Exception {
+		final List<Path> paths = FileUtils.getJarFiles(Paths.get(value), 1);
+		if (paths.isEmpty()) {
 			throw new IllegalArgumentException("Found no jar in "+value);
 		}
-		final URLClassLoader loader = URLClassLoader.newInstance(urls);
-		if (!JDbBackup.loadPlugins(loader)) {
-			throw new IllegalArgumentException("There is no plugins in "+value);
-		}
-		return loader;
+		return paths;
 	}
 }
--- src/main/java/com/fathzer/jdbbackup/cmd/JDbBackupCmd.java
+++ src/main/java/com/fathzer/jdbbackup/cmd/JDbBackupCmd.java
@@ -1,10 +1,15 @@
 package com.fathzer.jdbbackup.cmd;
 
 import java.io.IOException;
-import java.net.URLClassLoader;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.concurrent.Callable;
 
+import com.fathzer.jdbbackup.DestinationManager;
 import com.fathzer.jdbbackup.JDbBackup;
+import com.fathzer.jdbbackup.SourceManager;
+import com.fathzer.plugin.loader.PluginLoader;
+import com.fathzer.plugin.loader.jar.JarPluginLoader;
 import com.fathzer.plugin.loader.utils.ProxySettings;
 
 import picocli.CommandLine;
@@ -25,7 +30,7 @@ public class JDbBackupCmd implements Callable<Integer>, CommandLineSupport {
 	private ProxySettings proxy;
 	@Option(names={"-e","--extension"}, description="A jar file that contains an extension (DestinationManager or SourceManager), or a folder containing such jars",arity = "1..*",
 			converter = ExtensionSettingsConverter.class)
-	private URLClassLoader[] extensions;
+	private List<Path> extensions;
 	
 	/** Launches the command.
 	 * @param args The command arguments. Run the class without any arguments to know what are the available arguments.
@@ -41,8 +46,9 @@ public class JDbBackupCmd implements Callable<Integer>, CommandLineSupport {
 	@Override
 	public Integer call() throws Exception {
 		try {
-			loadExtensions();
-			new JDbBackup().backup(proxy, db, dest);
+			final JDbBackup backup = new JDbBackup();
+			loadExtensions(backup);
+			backup.backup(proxy, db, dest);
 			return ExitCode.OK;
         } catch (IllegalArgumentException e) {
         	err(e);
@@ -53,10 +59,17 @@ public class JDbBackupCmd implements Callable<Integer>, CommandLineSupport {
         }
 	}
 
-	private void loadExtensions() throws IOException {
-		for (URLClassLoader extension:extensions) {
-			System.out.println("We are here");
-			JDbBackup.loadPlugins(extension);
+	private void loadExtensions(JDbBackup backup) throws IOException {
+		final PluginLoader<Path> loader = new JarPluginLoader();
+		for (Path path : extensions) {
+			final List<SourceManager> sources = loader.getPlugins(path, SourceManager.class);
+			@SuppressWarnings("rawtypes")
+			final List<DestinationManager> destinations = loader.getPlugins(path, DestinationManager.class);
+			if (sources.isEmpty() && destinations.isEmpty()) {
+				throw new IOException("File "+path+" contains no extension");
+			}
+			backup.getSourceManagers().registerAll(sources);
+			backup.getDestinationManagers().registerAll(destinations);
 		}
 	}
 }
